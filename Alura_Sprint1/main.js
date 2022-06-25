17670237b002fc32edf7c1dfecaf873690320aa1
AlbertoXDEV <alberto.s.s.theodoro@gmail.com>
2022-06-25 18:01:43 -0300

    Finished the desktop version, expect for the footer

--- Alura_Sprint1/main.js
+++ Alura_Sprint1/main.js
@@ -1,22 +1,23 @@
 var input=document.querySelector(".text");
 var output="";
-const form=document.querySelector("textInput")
 const outputField=document.querySelector(".outputField")
-const validChars=/[a-z-\w\D\s]/g
+const validChars=/[a-z\s]/g
+const warningMsg=document.querySelector("#warning");
 
 function encrypt () {
 	if (input.value!="") {
 			outputField.textContent="";
 			var validArray=input.value.match(validChars);
 			if(validArray!=null){
+				updateErrorMsg(3)
 				output=input.value.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
 				outputField.textContent=output;
 				input.value="";
 			} else {
-				alert("Use somente letras minúsculas sem caractéres especiais");
+				updateErrorMsg(1)
 			}
 		}else {
-			alert("Por favor insira algum texto")
+			updateErrorMsg(2)
 		}
 	}
 
@@ -25,16 +26,42 @@ function decrypt () {
 			outputField.textContent="";
 			var validArray=input.value.match(validChars);
 			if(validArray!=null){
+				updateErrorMsg(3)
 				output=input.value.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
 				outputField.textContent=output;
 				input.value="";
 			} else {
-			alert("Use somente letras minúsculas sem caractéres especiais");
+				updateErrorMsg(1)
 				}
 		}else {
-			alert("Por favor insira algum texto")
+			updateErrorMsg(2)
 		}
 	}
+
+function updateErrorMsg(caseType) {
+	if (caseType==1) {
+		warningMsg.textContent="Use somente letras minúsculas sem caractéres especiais"
+		warningMsg.classList.add("txt-2")
+	}else if (caseType==2) {
+		warningMsg.textContent="Por favor insira algum texto"
+		warningMsg.classList.add("txt-2")
+	}else {
+		warningMsg.textContent=""
+	}
+}
+
+input.addEventListener('input',function (event) {
+		if (event.data!=null) {
+			var placeHolder=document.querySelector('#img')
+			placeHolder.classList.remove("img")
+			placeHolder.classList.add("hidden")
+		}else if (input.value=="") {
+			var placeHolder=document.querySelector('#img')
+			placeHolder.classList.remove("hidden")
+			placeHolder.classList.add("img")
+		}
+	}
+)
 
 var criptoButton = document.querySelector('#cripto')
 criptoButton.addEventListener('click',encrypt)
